e789b79814b2008c3802098375edf5f66e9c1b23
Nashira Deer <nashiradeer@gmail.com>
2024-10-11 09:57:06 -0300

    feat: port SwapchainSupportDetails to api2

--- src/api2/swapchain.rs
+++ src/api2/swapchain.rs
@@ -1,19 +1,78 @@
-use ash::vk;
+use ash::{khr::surface, prelude::*, vk};
 
-pub struct Swapchain2 {
-    pub image_views: Vec<vk::ImageView>,
-    pub framebuffers: Vec<vk::Framebuffer>,
-    pub swapchain: vk::SwapchainKHR,
-    pub render_pass: vk::RenderPass,
+/// Details about what the swapchain supports.
+#[derive(Clone, Default)]
+pub struct SwapchainSupportDetails {
+    /// The capabilities of the swapchain.
+    pub capabilities: vk::SurfaceCapabilitiesKHR,
+    /// The formats of the swapchain.
+    pub formats: Vec<vk::SurfaceFormatKHR>,
+    /// The present modes of the swapchain.
+    pub present_modes: Vec<vk::PresentModeKHR>,
 }
 
-impl Swapchain2 {
-    pub fn new() -> Self {
-        Self {
-            swapchain,
-            image_views,
-            framebuffers,
-            render_pass,
+impl SwapchainSupportDetails {
+    /// Query the swapchain support details.
+    pub fn query_support(
+        surface_instance: &surface::Instance,
+        surface: vk::SurfaceKHR,
+        device: vk::PhysicalDevice,
+    ) -> VkResult<SwapchainSupportDetails> {
+        let capabilities =
+            unsafe { surface_instance.get_physical_device_surface_capabilities(device, surface)? };
+
+        let formats =
+            unsafe { surface_instance.get_physical_device_surface_formats(device, surface)? };
+
+        let present_modes =
+            unsafe { surface_instance.get_physical_device_surface_present_modes(device, surface)? };
+
+        Ok(SwapchainSupportDetails {
+            capabilities,
+            formats,
+            present_modes,
+        })
+    }
+
+    /// Choose the format of the swapchain.
+    pub fn choose_format(&self) -> &vk::SurfaceFormatKHR {
+        for format in &self.formats {
+            if format.format == vk::Format::B8G8R8A8_SRGB
+                && format.color_space == vk::ColorSpaceKHR::SRGB_NONLINEAR
+            {
+                return format;
+            }
         }
+
+        &self.formats[0]
+    }
+
+    /// Choose the present mode of the swapchain.
+    pub fn choose_present_mode(&self) -> vk::PresentModeKHR {
+        for present_mode in &self.present_modes {
+            if *present_mode == vk::PresentModeKHR::MAILBOX {
+                return *present_mode;
+            }
+        }
+
+        vk::PresentModeKHR::FIFO
+    }
+
+    /// Choose the extent of the swapchain.
+    pub fn choose_extent(&self, width: u32, height: u32) -> vk::Extent2D {
+        let mut current_extent = vk::Extent2D { width, height };
+
+        current_extent.width = nalgebra::clamp(
+            current_extent.width,
+            self.capabilities.min_image_extent.width,
+            self.capabilities.max_image_extent.width,
+        );
+        current_extent.height = nalgebra::clamp(
+            current_extent.height,
+            self.capabilities.min_image_extent.height,
+            self.capabilities.max_image_extent.height,
+        );
+
+        current_extent
     }
 }
