86d759da57b28cc42e53477e6581fb1bfd343733
NashiraDeer <nashiradeer@gmail.com>
2025-10-26 19:15:16 -0300

    feat(vulkan): create module for errors

---
+++ src/vulkan/error.rs
@@ -0,0 +1,28 @@
+#![allow(non_upper_case_globals)]
+
+use std::fmt::{Display, Formatter, Result as FmtResult};
+
+use crate::vulkan::bindings::{
+    VkResult, VkResult_VK_ERROR_INCOMPATIBLE_DRIVER, VkResult_VK_SUCCESS,
+};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Error(VkResult);
+
+impl From<VkResult> for Error {
+    fn from(result: VkResult) -> Self {
+        Error(result)
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        match self.0 {
+            VkResult_VK_SUCCESS => write!(f, "VkResult::VK_SUCCESS"),
+            VkResult_VK_ERROR_INCOMPATIBLE_DRIVER => {
+                write!(f, "VkResult::VK_ERROR_INCOMPATIBLE_DRIVER")
+            }
+            err => write!(f, "Unknown VkResult error code: {}", err),
+        }
+    }
+}
