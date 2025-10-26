5123122f692f0008be9e759dcb46ece796d46177
NashiraDeer <nashiradeer@gmail.com>
2025-10-26 19:18:44 -0300

    chore(vulkan/error): allow non_snake_case on Error::fmt

--- src/vulkan/error.rs
+++ src/vulkan/error.rs
@@ -1,5 +1,3 @@
-#![allow(non_upper_case_globals)]
-
 use std::fmt::{Display, Formatter, Result as FmtResult};
 
 use crate::vulkan::bindings::{
@@ -16,6 +14,7 @@ impl From<VkResult> for Error {
 }
 
 impl Display for Error {
+    #[allow(non_snake_case)]
     fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
         match self.0 {
             VkResult_VK_SUCCESS => write!(f, "VkResult::VK_SUCCESS"),
