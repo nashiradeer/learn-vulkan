eede1437ea02517734b8a11c6c8bb79bdeca342f
NashiraDeer <nashiradeer@gmail.com>
2025-10-26 19:14:52 -0300

    feat(vulkan): create module file

--- src/vulkan/mod.rs
+++ src/vulkan/mod.rs
@@ -1,1 +1,17 @@
+use std::result::Result as StdResult;
+
 pub mod bindings;
+
+mod error;
+mod instance;
+mod sys;
+
+pub use error::*;
+pub use instance::*;
+pub use sys::*;
+
+pub fn vk_make_version(major: u32, minor: u32, patch: u32) -> u32 {
+    (major << 22) | (minor << 12) | patch
+}
+
+pub type Result<T> = StdResult<T, Error>;
